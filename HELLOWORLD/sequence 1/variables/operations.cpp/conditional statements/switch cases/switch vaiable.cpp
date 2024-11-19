ae1dacefbd476c846f130b007565bd53954ea6d1
vimunga <victoria.nganga@strathmore.edu>
2024-11-19 19:26:46 +0300

    updates

--- HELLOWORLD/sequence 1/variables/operations.cpp/conditional statements/switch cases/switch vaiable.cpp
+++ HELLOWORLD/sequence 1/variables/operations.cpp/conditional statements/switch cases/switch vaiable.cpp
@@ -11,6 +11,39 @@ int main()
     {
         case 1:
         cout <<" Monday Classes\n.............." << endl;
-        cout <<"\t1`"
+        cout <<"\t1. BBIT 1106:  Math for Biz" << endl;
+        cout << "\t2. BBIT 1102: Intro to programming" << endl;
+        break;
+
+        
+        case 2:
+        cout <<" Tuesday Classes\n.............." << endl;
+        cout <<"\t1. BBIT 1102:  Math for Biz" << endl;
+        cout << "\t2. BBIT 1104: Ethics" << endl;
+        break;
+
+        case 3:
+        cout <<" Wednesday Classes\n.............." << endl;
+        cout <<"\t1. BBIT 1106:  Math for Biz" << endl;
+        cout << "\t2. BBIT 1104: Theology" << endl;
+        cout << "\t3. BBIT 1101: Fundamentals of IT" << endl;
+        break;
+
+        case 4:
+        cout <<" Thursday Classes\n.............." << endl;
+        cout <<"\t1. BBIT 1105:  Accounting" << endl;
+        cout << "\t2. BBIT 1101: Fundamentals of IT" << endl;
+        break;
+        
+         case 5:
+        cout <<" Friday Classes\n.............." << endl;
+        cout <<"\t1. BBIT 1103: Business  Computing" << endl;
+        break;
+
+
+        default:
+        cout <<" Invalid Input";
+
     }
+    return 0;
 }
